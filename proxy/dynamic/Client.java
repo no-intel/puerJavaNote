064e332b49065d81580b64ed3a04d903153161f4
no-int <tkdduf212@gmail.com>
2023-08-16 00:14:32 +0900

    dynamic proxy
    (cherry picked from commit 187f9893748e55916fa224672281c7f15a8e9d50)

---
+++ proxy/dynamic/Client.java
@@ -0,0 +1,16 @@
+package proxy.dynamic;
+
+import java.lang.reflect.Proxy;
+
+public class Client {
+    public static void main(String[] args) {
+        Service service = (Service) Proxy.newProxyInstance(
+                Service.class.getClassLoader(),
+                new Class[]{Service.class},
+                new ServiceProxy()
+        );
+
+        service.select();
+        service.save();
+    }
+}
