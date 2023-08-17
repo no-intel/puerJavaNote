e1fb5b9cc2ba74c21c8670cbd916103cd1e30958
no-int <tkdduf212@gmail.com>
2023-08-18 00:54:30 +0900

    type eresure

---
+++ typeEresure/Te.java
@@ -0,0 +1,14 @@
+package typeEresure;
+
+public class Te {
+    public static class Test implements Comparable<Integer> {
+        @Override
+        public int compareTo(Integer o) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
